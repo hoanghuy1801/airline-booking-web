253fb02b664ce4107f86e438ab3afb062f718a2b
Phạm Lê Khánh Duy <phamduy2408@gmail.com>
2023-10-15 13:13:01 +0700

    fix lastName and firstName not available

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,139 +1,216 @@
-
-import { useNavigate } from 'react-router-dom';
-import React, { useState } from 'react';
-import { MenuOutlined, UserOutlined } from '@ant-design/icons';
-import { Drawer, Select, Row, Col, Button, Typography } from 'antd';
-import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react'
+import { MenuOutlined, UserOutlined } from '@ant-design/icons'
+import { Drawer, Select, Row, Col, Button, Typography } from 'antd'
+import { useDispatch, useSelector } from 'react-redux'
 import '../Header/Header.css'
 import logo from '../../assets/VivuAirlines.png'
 
-import { useLanguage, LanguageProvider } from '../../LanguageProvider/LanguageProvider';
-import { setLanguage } from '../../redux/reducers/languageSlice';
+import { useLanguage, LanguageProvider } from '../../LanguageProvider/LanguageProvider'
+import { setLanguage } from '../../redux/reducers/languageSlice'
 
-const { Title, Text } = Typography;
+const { Title, Text } = Typography
 const Header = () => {
-    const navigate = useNavigate();
-    const dispastch = useDispatch();
-    const [openMenu, setOpenMenu] = useState(false);
+    const navigate = useNavigate()
+    const dispastch = useDispatch()
+    const [openMenu, setOpenMenu] = useState(false)
 
     const AppMenuDrawer = ({ isInline = false }) => {
         return (
             <>
                 <Row>
-                    <div style={{
-                        paddingLeft: '20%'
-                    }}> <img src={logo} onClick={() => { navigate('/') }} style={{ cursor: 'pointer' }} /></div>
-                </Row>
-                <Row>
-                    <div className='auth'>
-                        <UserOutlined className='auth-child' />
-                        <span className='auth-child' onClick={() => { navigate('/register') }} >Đăng ký </span>
-                        <span>|</span>
-                        <span className='auth-child' onClick={() => { navigate('/login') }}>Đăng nhập</span>
+                    <div
+                        style={{
+                            paddingLeft: '20%'
+                        }}
+                    >
+                        <img
+                            src={logo}
+                            onClick={() => {
+                                navigate('/')
+                            }}
+                            style={{ cursor: 'pointer' }}
+                        />
                     </div>
                 </Row>
                 <Row>
-                    <Button className='service-child' type='link'>CHUYẾN BAY CỦA TÔI</Button>
-                    <Button className='service-child' type='link' onClick={() => { navigate('/checkin') }}>CHECKIN-ONLINE</Button>
+                    <div className="auth">
+                        <UserOutlined className="auth-child" />
+                        <span
+                            className="auth-child"
+                            onClick={() => {
+                                navigate('/register')
+                            }}
+                        >
+                            Đăng ký
+                        </span>
+                        <span>|</span>
+                        <span
+                            className="auth-child"
+                            onClick={() => {
+                                navigate('/login')
+                            }}
+                        >
+                            Đăng nhập
+                        </span>
+                    </div>
+                </Row>
+                <Row>
+                    <Button className="service-child" type="link">
+                        CHUYẾN BAY CỦA TÔI
+                    </Button>
+                    <Button
+                        className="service-child"
+                        type="link"
+                        onClick={() => {
+                            navigate('/checkin')
+                        }}
+                    >
+                        CHECKIN-ONLINE
+                    </Button>
                 </Row>
             </>
         )
     }
     const AppMenu = ({ isInline = false }) => {
-        const { getText, changeLanguage } = useLanguage();
-        const dispastch = useDispatch();
+        const { getText, changeLanguage } = useLanguage()
+        const dispastch = useDispatch()
         const handleChangeLanguageVI = () => {
-            changeLanguage('vi');
+            changeLanguage('vi')
             dispastch(setLanguage('vi'))
         }
         const handleChangeLanguageEN = () => {
-            changeLanguage('en');
-            dispastch(setLanguage('en'));
+            changeLanguage('en')
+            dispastch(setLanguage('en'))
         }
-        const isAuthenticated = useSelector(state => state.Auth.isAuthenticated);
-        const InforUser = useSelector(state => state.Auth.InforUser);
+        const isAuthenticated = useSelector((state) => state.Auth.isAuthenticated)
+        const InforUser = useSelector((state) => state.Auth.InforUser)
         return (
             <>
-                <div className='menuPage'>
-                    <Row className='headerMenu'>
-                        <Col span={6} >
-                            <div className='logo'>  <img src={logo} onClick={() => { navigate('/') }} style={{ cursor: 'pointer' }} /></div>
-                        </Col>
-                        <Col span={10} >
-                            <div className='service'>
-                                <Button className='service-child' type='link' onClick={() => { navigate('/my/search-booking') }}>{getText('myflight')}</Button>
-                                <Button className='service-child' type='link' onClick={() => { navigate('/checkin') }}>CHECKIN-ONLINE</Button>
-                                <Button className='service-child' type='link' onClick={() => { navigate('/admins') }}>Admin</Button>
+                <div className="menuPage">
+                    <Row className="headerMenu">
+                        <Col span={6}>
+                            <div className="logo">
+                                <img
+                                    src={logo}
+                                    onClick={() => {
+                                        navigate('/')
+                                    }}
+                                    style={{ cursor: 'pointer' }}
+                                />
                             </div>
                         </Col>
-                        <Col span={4} >
-                            <div className='auth'>
-                                <UserOutlined className='auth-Icon' />
-                                {isAuthenticated === false ?
-                                    <> <span className='auth-child' onClick={() => { navigate('/register') }} >{getText('register')} </span>
-                                        <span className='auth-Icon'>|</span>
-                                        <span className='auth-child' onClick={() => { navigate('/login') }}>{getText('login')} </span>
-                                    </>
-                                    :
-                                    <>
-                                        <Text className='auth-name' onClick={() => navigate('/profile/account')}>{InforUser.lastName} {InforUser.firstName}</Text>
-                                    </>
-                                }
+                        <Col span={10}>
+                            <div className="service">
+                                <Button
+                                    className="service-child"
+                                    type="link"
+                                    onClick={() => {
+                                        navigate('/my/search-booking')
+                                    }}
+                                >
+                                    {getText('myflight')}
+                                </Button>
+                                <Button
+                                    className="service-child"
+                                    type="link"
+                                    onClick={() => {
+                                        navigate('/checkin')
+                                    }}
+                                >
+                                    CHECKIN-ONLINE
+                                </Button>
+                                <Button
+                                    className="service-child"
+                                    type="link"
+                                    onClick={() => {
+                                        navigate('/admins')
+                                    }}
+                                >
+                                    Admin
+                                </Button>
                             </div>
                         </Col>
                         <Col span={4}>
-                            <div className='translation'>
-                                <span className='auth-child' onClick={() => handleChangeLanguageVI()} >VI </span>
-                                <span className='auth-child' onClick={() => handleChangeLanguageEN()}>EN</span>
+                            <div className="auth">
+                                <UserOutlined className="auth-Icon" />
+                                {isAuthenticated === false ? (
+                                    <>
+                                        <span
+                                            className="auth-child"
+                                            onClick={() => {
+                                                navigate('/register')
+                                            }}
+                                        >
+                                            {getText('register')}
+                                        </span>
+                                        <span className="auth-Icon">|</span>
+                                        <span
+                                            className="auth-child"
+                                            onClick={() => {
+                                                navigate('/login')
+                                            }}
+                                        >
+                                            {getText('login')}
+                                        </span>
+                                    </>
+                                ) : (
+                                    <>
+                                        <Text className="auth-name" onClick={() => navigate('/profile/account')}>
+                                            {InforUser?.lastName} {InforUser?.firstName}
+                                        </Text>
+                                    </>
+                                )}
+                            </div>
+                        </Col>
+                        <Col span={4}>
+                            <div className="translation">
+                                <span className="auth-child" onClick={() => handleChangeLanguageVI()}>
+                                    VI
+                                </span>
+                                <span className="auth-child" onClick={() => handleChangeLanguageEN()}>
+                                    EN
+                                </span>
                             </div>
                         </Col>
                     </Row>
-
-                </div >
+                </div>
             </>
-
-
         )
     }
     return (
         <LanguageProvider>
-            <div className='header' >
-                <div
-                    className='menuIcon' >
-                    <MenuOutlined style={{
-                        color: "white",
-                        fontSize: 30,
-                        paddingLeft: 12,
-                        paddingTop: 12
-                    }}
+            <div className="header">
+                <div className="menuIcon">
+                    <MenuOutlined
+                        style={{
+                            color: 'white',
+                            fontSize: 30,
+                            paddingLeft: 12,
+                            paddingTop: 12
+                        }}
                         onClick={() => {
-                            setOpenMenu(true);
+                            setOpenMenu(true)
                         }}
                     />
-
                 </div>
-                <span className='headerMenu'>
+                <span className="headerMenu">
                     <AppMenu />
                 </span>
                 <Drawer
-                    placement='left'
+                    placement="left"
                     open={openMenu}
                     closable={false}
                     onClose={() => {
-                        setOpenMenu(false);
+                        setOpenMenu(false)
                     }}
                     bodyStyle={{ backgroundColor: '#006885' }}
                 >
                     <AppMenuDrawer />
                 </Drawer>
-
-
-
-            </div >
+            </div>
         </LanguageProvider>
-
-
-    );
+    )
 }
 
-export default Header;
+export default Header
